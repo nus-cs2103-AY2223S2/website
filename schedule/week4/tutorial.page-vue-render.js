070e4ed0c9c8f6cebc0795d7f77a0264ae024e04
Damith C. Rajapakse <damith@gmail.com>
2023-01-01 03:28:46 +0800

    Site Update. [skip ci]

--- schedule/week4/tutorial.page-vue-render.js
+++ schedule/week4/tutorial.page-vue-render.js
@@ -26,6 +26,6 @@ with(this){return _c('div',[_c('ul',[_c('li',[_v("Show your collaborative projec
 with(this){return _c('ul',[_c('li',[_v("Do the first of the two exercise given below, as directed by the tutor. If there is time left, do the second one as well.")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.2")]),_v(", generated on Sat, 31 Dec 2022, 2:14:48 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.2")]),_v(", generated on Sun, 1 Jan 2023, 3:24:36 GMT+8]")])])])}
 }];
   
