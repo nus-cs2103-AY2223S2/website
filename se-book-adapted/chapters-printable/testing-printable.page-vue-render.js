c10c3654dd34dabf49cc5f6d560a2c8a1b69679e
Damith C. Rajapakse <damith@gmail.com>
2023-01-19 23:58:30 +0800

    Site Update. [skip ci]

--- se-book-adapted/chapters-printable/testing-printable.page-vue-render.js
+++ se-book-adapted/chapters-printable/testing-printable.page-vue-render.js
@@ -206,6 +206,6 @@ with(this){return _c('div',[_c('h2',{attrs:{"id":"dependency-injection"}},[_c('s
 with(this){return _c('strong',[_c('em',[_v("Dependency injection")]),_v(" is the process of 'injecting' objects to replace current dependencies with a different object.")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.2")]),_v(", generated on Tue, 17 Jan 2023, 12:26:36 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.2")]),_v(", generated on Thu, 19 Jan 2023, 23:53:59 GMT+8]")])])])}
 }];
   
