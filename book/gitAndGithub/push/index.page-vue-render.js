4e6d9979445ac8d8ca44c2ba9e79cb2b252072b9
damithc <damith@github>
2023-03-30 00:08:06 +0800

    Site Update. [skip ci]

--- book/gitAndGithub/push/index.page-vue-render.js
+++ book/gitAndGithub/push/index.page-vue-render.js
@@ -41,6 +41,6 @@ with(this){return _c('ol',[_c('li',[_c('p',[_v("Login to your GitHub account and
 with(this){return _c('p',[_c('strong',[_v("3. Push the repo")]),_v(" to the remote.")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.1.0")]),_v(", generated on Sat, 25 Mar 2023, 23:31:49 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.1.0")]),_v(", generated on Thu, 30 Mar 2023, 0:03:38 GMT+8]")])])])}
 }];
   
