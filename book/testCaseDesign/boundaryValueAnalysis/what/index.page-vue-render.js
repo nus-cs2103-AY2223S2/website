8582d0c7a4907b6b8ec951e15455d9eac369a52e
damithc <damith@github>
2023-04-03 21:14:26 +0800

    Site Update. [skip ci]

--- book/testCaseDesign/boundaryValueAnalysis/what/index.page-vue-render.js
+++ book/testCaseDesign/boundaryValueAnalysis/what/index.page-vue-render.js
@@ -23,6 +23,6 @@ with(this){return _c('p',[_c('strong',[_v("BVA suggests that when picking test i
 with(this){return _c('p',[_v("Boundary values are sometimes called "),_c('em',[_v("corner cases")]),_v(".")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.1.0")]),_v(", generated on Mon, 3 Apr 2023, 21:00:37 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.1.0")]),_v(", generated on Mon, 3 Apr 2023, 21:10:41 GMT+8]")])])])}
 }];
   
